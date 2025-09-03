c17b1ee9953c199821feac4a9c8587d85ff6d60d
Azarifar.Mo <capricornazarifar@gmail.com>
2025-09-03 15:54:15 +0330

    dev: start from scratch.

--- src/auth.py
+++
@@ -1,8 +0,0 @@
-class AuthStrategy:
-    def authenticate(self, username, password):
-        raise NotImplementedError
-
-
-class MockAuthStrategy(AuthStrategy):
-    def authenticate(self, username, password):
-        return username and password and len(username) > 5 and len(password) > 5
